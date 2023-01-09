b2839bdc930a024f6b38572f1a5f91e70baa931d
emup <emuppet@protonmail.com>
2023-01-09 10:15:40 +0100

    Added react router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 import ContactPage from "./pages/contact-page";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import SignupPage from './pages/signup/signup-page';
 
 function App() {
   return (
-      <div>
-        <Route path="/contact">
-            <ContactPage/>
-        </Route>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="contact" element={ <ContactPage/> } />
+        <Route path="signup" element={ <SignupPage/> } />
+      </Routes>
+    </div>
   );
 }
 
