5acab6f314540351231c359d9dbb391961ece5ee
emup <emuppet@protonmail.com>
2023-01-14 22:08:00 +0100

    The "I did something" award for @Pjoess

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import './App.css';
+import "./App.css"
 import { Routes, Route } from "react-router-dom"
-import ContactPage from "./pages/contact/contact-page";
-import SignupPage from './pages/signup/signup-page';
+import ContactPage from "./pages/contact/contact-page"
+import SignupPage from "./pages/signup/signup-page"
 import CartPage from "./pages/cart/cart-page"
-import IndexPage from './pages/index/index-page';
-import AboutUs from './pages/index/aboutUs-page';
-import Tickets from './pages/index/ticket-page';
-import LoginPage from './pages/login/login-page'
-import Programming from './pages/Programmering/programming';
+import IndexPage from "./pages/index/index-page"
+import AboutUs from "./pages/index/about-us-page"
+import Tickets from "./pages/ticket/ticket-page"
+import LoginPage from "./pages/login/login-page"
+import Programming from "./pages/Programmering/programming"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<IndexPage/>} />
-        <Route path="contact" element={ <ContactPage/> } />
-        <Route path="cart" element={ <CartPage/> } />
-        <Route path="aboutus" element={ <AboutUs/> } />
-        <Route path="tickets" element={ <Tickets/> } />
-        <Route path="signup" element={ <SignupPage/> } />
-        <Route path="login" element={ <LoginPage/> } />
-        <Route path="programmering" element={ <Programming/> } />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="aboutus" element={<AboutUs />} />
+        <Route path="tickets" element={<Tickets />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="programmering" element={<Programming />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
